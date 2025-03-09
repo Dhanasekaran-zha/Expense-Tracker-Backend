d84dc512abf37ee7606e25a0bbd59fe1b4e72be3
Dhanasekaran-zha <dhanasekaran_zha@yahoo.com>
2025-03-09 10:08:23 +0530

    Added monthly api for dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -73,3 +73,93 @@ exports.getDashboardData = async (req, res) => {
         res.status(500).json({message: "Server Error", error: e.message})
     }
 }
+
+//Monthly Dashboard Data
+exports.getMonthlyDashboardData = async (req, res) => {
+    try {
+        const userId = req.user.id
+        const userObjectId = new Types.ObjectId(String(userId))
+
+        const {year, month} = req.params;
+
+        // Convert year and month to numbers
+        const y = parseInt(year);
+        const m = parseInt(month);
+
+        if (isNaN(y) || isNaN(m) || m < 1 || m > 12) {
+            return res.status(400).json({message: "Invalid year or month"});
+        }
+
+        // Get start and end of the month
+        const startDate = new Date(y, m - 1, 1); // First day of the month
+        const endDate = new Date(y, m, 1); // First day of the next month
+
+        //Fetch total income & expense of the month
+        const totalIncome = await Income.aggregate([
+            {
+                $match: {
+                    userId: userObjectId, date: {
+                        $gte: startDate,
+                        $lt: endDate,       // Start of the next month
+                    }
+                }
+            },
+            {$group: {_id: null, total: {$sum: "$amount"}}}
+        ])
+
+        const totalExpense = await Expense.aggregate([
+            {
+                $match: {
+                    userId: userObjectId, date: {
+                        $gte: startDate,
+                        $lt: endDate,       // Start of the next month
+                    }
+                },
+            },
+            {$group: {_id: null, total: {$sum: "$amount"}}}
+        ])
+
+        // current month expenses
+        const monthlyIncome = await Income.find({
+            userId,
+            date: {$gte: startDate, $lt: endDate}
+        }).sort({date: -1});
+
+        // monthly expenses
+        const monthlyExpense = await Expense.find({
+            userId,
+            date: {$gte: startDate, $lt: endDate}
+        }).sort({date: -1});
+
+        //Fetch last 5 transactions (income + expense)
+        const lastTransactions = [
+            ...(await Income.find({
+                userId,
+                date: {$gte: startDate, $lt: endDate}
+            }).sort({date: -1}).limit(5)).map((transaction) => ({
+                ...transaction.toObject(),
+                type: "income"
+            })),
+            ...(await Expense.find({
+                userId,
+                date: {$gte: startDate, $lt: endDate}
+            }).sort({date: -1}).limit(5)).map((transaction) => ({
+                ...transaction.toObject(),
+                type: "expense"
+            }))
+        ].sort((a, b) => b.date - a.date)
+
+        // Final Response
+        res.status(200).json({
+            totalBalance: (totalIncome[0]?.total || 0) - (totalExpense[0]?.total || 0),
+            totalIncome: totalIncome[0]?.total || 0,
+            totalExpense: totalExpense[0]?.total || 0,
+            monthlyExpenses: monthlyExpense,
+            monthlyIncome: monthlyIncome,
+            recentTransactions: lastTransactions
+        })
+
+    } catch (e) {
+        res.status(500).json({message: "Server Error", error: e.message})
+    }
+}
--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const {protect} = require('../middleware/authMiddleware')
-const {getDashboardData} = require('../controllers/dashboardController')
+const {getDashboardData, getMonthlyDashboardData} = require('../controllers/dashboardController')
 
 const router = express.Router()
 
 router.get('/', protect, getDashboardData)
+router.get('/get/:year/:month', protect, getMonthlyDashboardData)
+
 
 module.exports = router
