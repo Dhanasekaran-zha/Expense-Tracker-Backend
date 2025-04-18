2b2ff9bd791c8a40f496a23a6063fdb6cf630dd0
Dhanasekaran-zha <dhanasekaran_zha@yahoo.com>
2025-04-18 11:40:06 +0530

    cors modified

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(
     cors({
         origin: process.env.CLIENT_URL || "*",
         methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content—Type ", "Authorization"],
+        allowedHeaders: ["Content-Type ", "Authorization"],
     }))
 
 app.use(express.json())
